a15a4483b2fc8aa8b0bfc775c0eed8f71ff6d0d5
root <root@vps03601.eskiz.uz>
2024-09-12 16:54:33 +0500

    Initial commit from Ubuntu server

--- myshop/settings.py
+++ myshop/settings.py
@@ -113,7 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Корневая папка для статических файлов
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Убедись, что это правильный путьSTATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Убедись, что это правильный путь
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
